0006980eae3ef221550b02ba9de5b2a444e82249
mytreo <mytreogm@gmail.com>
2015-11-05 20:23:06 +0200

    Initialize rep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestProject::Application.config.secret_key_base = 'cdcbd36fe17838409d8a203d3879e9082d10708b8711d18f3cd18fdaa3fe4510875e815a809333889ab5468f900ac65ff3fd00570b03e538a9a0ee979cc37e5a'
