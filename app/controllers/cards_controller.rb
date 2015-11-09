21f3dd8e512a723176242d1a99d68d7e0eae1a64
mytreo <mytreogm@gmail.com>
2015-11-09 22:40:28 +0200

    add controller

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,45 @@
+class CardsController < ApplicationController
+  
+  def index
+    @cards = Card.all
+  end
+  
+  def new
+    @card = Card.new
+  end
+  
+  def create
+    @card = User.new(card_params)    
+    if @card.save
+      redirect_to @card
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+     @card = Card.find(params[:id])
+    if @card.update_attributes(card_params)
+      redirect_to @card
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    Card.find(params[:id]).destroy
+    redirect_to root_url
+  end
+  
+  
+    private
+
+    def card_params
+      params.require(:card).permit(:text_ru, :text_en, :review_date)
+    end
+  
+  
+end
