dcd66fab48cf4243fe9e766c09f12081e3d50310
mytreo <mytreogm@gmail.com>
2015-11-09 22:10:37 +0200

    add model Card

---
+++ spec/models/card_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+
+describe Card do
+
+
+ before do
+  @card= Card.new(text_ru: "example ru", text_en: "example en", 
+                  review_date: Date.today)
+  end
+
+ subject { @card }
+
+ it { should respond_to(:text_ru) }
+  it { should respond_to(:text_en) }
+  it { should respond_to(:review_date) }
+
+
+end
