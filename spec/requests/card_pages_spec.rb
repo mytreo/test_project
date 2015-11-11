3c3595c5f191d5de397b18a82169e178e4f905c8
mytreo <mytreogm@gmail.com>
2015-11-11 21:58:50 +0200

    tests

--- spec/requests/card_pages_spec.rb
+++ spec/requests/card_pages_spec.rb
@@ -10,24 +10,72 @@ describe "Card pages" do
 
 
  describe "index" do
- #  visit root_path
- it { should have_title('Index') }
+  before do
+    visit root_path #'/' 
+  end
+  it { should have_title('Index') }
+    describe "cards watch" do
+      before(:all) { 30.times { FactoryGirl.create(:card) } }
+      after(:all)  { Card.delete_all }
+       it "should list all cards" do
+          Card.all.each do |card|
+           expect(page).to have_selector('span', text: "Card# #{card.id}")
+         end      
+       end
+    end
  end
  
-#  describe "show" do
-#   visit card_path
-#   it { should have_title('Show') }
-# end
+  describe "show" do
+   before do 
+      visit card_path(card)
+   end
+   it { should have_title('Show') }
+   it { should have_selector('span', text: "Card# #{card.id}")}
+   it { should have_link('edit', href: edit_card_path(card)) }
+
+   
+   describe "destroy action" do
+              before { delete card_path(FactoryGirl.create(:card)) }
+              specify { expect(response).to redirect_to(root_path) }
+   end
+ end
  
- # describe "new" do
- #  visit new_card_path
- #  it { should have_title('New') }
- #end
+  describe "new" do
+   before do
+     visit new_card_path
+   end  
+   it { should have_title('New') }
+     
+     describe "try to create" do
+      before do
+        fill_in "card_text_ru",         with: "example ru"
+        fill_in "card_text_en",        with: "example en"
+      end
+
+      it "should create a card" do
+        expect { click_button "Create Card" }.to change(Card, :count).by(1)
+      end
+   end
+ end
  
-# describe "edit" do
- #  visit edit_card_path(:card)
-#   it { should have_title('Edit') }
-# end
+ describe "edit" do
+  before do
+    visit edit_card_path(card)
+  end
+  it { should have_title('Edit') }
+  
+   describe "try to edit" do
+      let(:new_text_ru)  { "New text" }
+      before do
+        fill_in "card_text_ru",         with: new_text_ru
+        click_button "Edit Card" 
+      end
+       it { should have_selector('span', text: "Card# #{card.id}") }
+
+      specify { expect(card.reload.text_ru).to  eq new_text_ru }
+   end
+
+ end
  
 
 
