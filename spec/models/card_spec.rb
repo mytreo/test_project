0b3d8318494c4ccf447f03b102c9532c8a695b4f
mytreo <mytreogm@gmail.com>
2015-11-17 23:29:44 +0200

    fixtest

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
-
 describe Card do
 
-
- before do
-  @card= Card.new(text_ru: "example ru", text_en: "example en", 
-                  review_date: Date.today)
+  before do
+    @card= Card.new(text_ru: "example ru", text_en: "example en",
+                    review_date: Date.today)
   end
 
- subject { @card }
+  subject { @card }
 
- it { should respond_to(:text_ru) }
+  it { should respond_to(:text_ru) }
   it { should respond_to(:text_en) }
   it { should respond_to(:review_date) }
-
 
 end
